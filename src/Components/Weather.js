5f3f969952b2d726823ec487aeef95d40a7ae973
leonid <leonid.karachevtsev@gmail.com>
2019-01-27 12:10:29 +0300

    add url key

---
+++ src/Components/Weather.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Weather extends React.Component {
+    render() {
+        return (
+            <div>FOR NIGGAZ</div>
+        );
+    }
+}
+
+export default Weather;
