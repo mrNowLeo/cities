3e5ed157d0d57e30ec2a0930af0dcbb8aa01fe9e
leonid <leonid.karachevtsev@gmail.com>
2019-01-27 11:51:04 +0300

    add Info

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Info from './Components/Info';
+
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <Info />
+      </div>
+    );
+  };
+} 
+
+export default App;
---
+++ src/Components/Info.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Info extends React.Component {
+    render() {
+        return (
+            <div> 
+                <h2>Weather for real fucking niggaz</h2>
+            </div>
+        );
+    }
+}
+
+export default Info;
